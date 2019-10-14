5d5bbf452eb7e3ce3ae34030e0d3706d6bd10f87
roee030 <roee.angel93@gmail.com>
2019-10-14 23:59:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import DisplayWord from './components/DisplayWord'
 import './App.css'
 export default class App extends Component {
   state = {
-    word: Words[Math.floor(Math.random()* Words.length)]
+    word: Words[Math.floor(Math.random()* Words.length)],
+    guessedLetters: []
   }
   render() {
     return (
       <div className="App">
         <h1>Hangman</h1>
-        <DisplayWord word={this.state.word}/>
+        <DisplayWord word={this.state.word} guessedLetters = {this.state.guessedLetters}/>
       </div>
     )
   }
