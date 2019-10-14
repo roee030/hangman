0cd079007cbcd055f3388d1076fcec698bca4b25
roee030 <roee.angel93@gmail.com>
2019-10-14 23:56:19 +0300

    add style

--- src/components/WordStyle.js
+++ src/components/WordStyle.js
@@ -3,7 +3,7 @@ import './WordStyle.css'
 export default function WordStyle({word}) {
     return (
         <div className="wordWrapper" >
-            <h1 >{word}</h1>
+            {word}
         </div>
     )
 }
